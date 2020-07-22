1e6f31a29c6d758adeb8071ae3642a81cb94e606
Muhammad Moiz Saeed <45221245+moiztechsavvy@users.noreply.github.com>
2020-07-22 11:39:50 -0400

    Update inputValidation.ts

--- backend/src/HelperFunctions/inputValidation.ts
+++ backend/src/HelperFunctions/inputValidation.ts
@@ -1,7 +1,3 @@
-let sqlInjection = (input: string) => {
-    //Return true if input is okay, false if the input is an injection.
-    return true;
-}
 
 //Validates email string against regex
 //after checking for SQL Injection.
