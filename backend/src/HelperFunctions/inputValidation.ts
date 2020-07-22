390c464db01ae79228c70a3c3682986bca8798ee
k-specht <xpecht@gmail.com>
2020-07-22 04:40:01 +0000

    Added injection prevention func

---
+++ backend/src/HelperFunctions/inputValidation.ts
@@ -0,0 +1,7 @@
+// Usage: Returns true if no common sql injections are detected in the input string.
+// DISCLAIMER: The only reliable way to prevent malicious SQL injection is parameterized queries. This function is redundant.
+let sqlInjection = (input: string) => {
+    // Reg. Exp. check for quotation-bound and comment-escape sql code
+    const injectTester = /^.*'.*([or]|[and]|[\*]|[-]).*'.*$/;
+    return !injectTester.test(input.toLowerCase());
+}
