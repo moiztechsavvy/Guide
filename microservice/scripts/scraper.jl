d3997ee8bebb194a66fc9cdf78b7b44ae3899289
lhnguyen-vn <lhnguyen.vn@gmail.com>
2020-07-30 16:13:07 -0400

    Add dataset.csv file

--- microservice/scripts/scraper.jl
+++ microservice/scripts/scraper.jl
@@ -1,8 +1,14 @@
 using HTTP, Gumbo, Cascadia
+using DelimitedFiles
 
 const PROTOCOL = "http://"
 const DOMAIN = "www.dermnet.com"
 const DIR_PAGE = "/dermatology-pictures-skin-disease-pictures"
+
+function buildurl(url)
+    url = replace(url, r"\s" => "%20")
+    startswith(url, "/") ? PROTOCOL * DOMAIN * url : url
+end
 
 function fetchpage(url)
     url = buildurl(url)
@@ -15,40 +21,42 @@ function fetchpage(url)
     return Gumbo.parsehtml(content)
 end
 
-buildurl(url) = startswith(url, "/") ? PROTOCOL * DOMAIN * url : url
-
 function extract_classes()
     dom = fetchpage(DIR_PAGE)
     table = eachmatch(Selector("table"), dom.root)[1]
     classes = eachmatch(Selector("a[href^='/images/']"), table)
-    return Dict(text(c) => extract_subclasses(c.attributes["href"]) for c in classes)
+    return Dict(text(c) => c.attributes["href"] for c in classes)
 end
 
 function extract_subclasses(url)
     dom = fetchpage(url)
     subclasses = eachmatch(Selector("a[href^='/images/']"), dom.root)
-    return Dict(text(s) => extract_images(s.attributes["href"]) for s in subclasses)
+    return Dict(text(s) => s.attributes["href"] for s in subclasses)
 end
 
 function extract_images(url)
     dom = fetchpage(url)
     images = eachmatch(Selector("img[src^='$(PROTOCOL * DOMAIN)']"), dom.root)
-    return [img.attributes["src"] for img in images]
+    return [(img.parent.attributes["name"], img.attributes["src"]) for img in images]
 end
 
 function scrape(ospath)
-    classes = extract_classes()
-    for (class, subclasses) in classes
-        for (subclass, imgurls) in subclasses
-            path = mkpath("$ospath/$class/$subclass/")
-            for imgurl in imgurls
+    io = open(joinpath(ospath, "dataset.csv"), "w")
+
+    for (class, class_url) in extract_classes()
+        class = replace(class, r"\s*photos"i => "")
+        for (subclass, subclass_url) in extract_subclasses(class_url)
+            subclass = replace(subclass, r"\s*photos"i => "")
+            for (img, img_url) in extract_images(subclass_url)
                 try 
-                    imgurl = buildurl(imgurl)
-                    HTTP.download(imgurl, path)
+                    HTTP.download(buildurl(img_url), ospath)
+                    writedlm(io, [img class subclass], ",")
                 catch e
-                    println(e)
+                    @warn(e)
                 end
             end
         end
     end
+    
+    close(io)
 end
