04f3fdc1a196097fc8cc9198d1364e26353496b4
lhnguyen-vn <lhnguyen.vn@gmail.com>
2020-08-03 18:22:48 -0400

    Adjust VAE model and loss function

--- microservice/training/preprocessing.py
+++ microservice/training/preprocessing.py
@@ -1,41 +1,63 @@
-import cv2
 import numpy as np
 import pandas as pd
+from PIL import Image
 import os
+import torch
+import torchvision.transforms as T
 from torch.utils.data import Dataset
 
 IMG_PATH = 'assets/'
 DATASET_PATH = 'dataset.csv'
+TRANSFORM = T.Compose([
+    T.Resize(256),
+    T.CenterCrop(224),
+    T.ToTensor(),
+    T.Normalize(mean=[0.485, 0.456, 0.406],
+        std=[0.229, 0.224, 0.225])
+])
 
 
 class DermDataset(Dataset):
-    def __init__(self, dataset, img_dir=IMG_PATH):
+    def __init__(self, dataset, img_dir=IMG_PATH, transform=None, label=True):
         super().__init__()
-        self.dataset = dataset
+        self.dataset = dataset.values
         self.img_dir = img_dir
+        self.transform = transform
+        self.label = label
 
     def __len__(self):
-        return len(dataset.index)
+        return len(self.dataset)
 
     def __getitem__(self, index):
-        row = self.dataset.iloc[index]
-        img_name, label = row.img_name, row.label
+        img_name, img_label, img_sublabel = self.dataset[index]
         img_path = os.path.join(self.img_dir, img_name)
-        image = cv2.imread(img_path, 0)
-        return standardize(image), label
+        image = Image.open(img_path)
+        if self.transform is not None:
+            image = self.transform(image)
+        if self.label:
+            return image, img_label
+        else:
+            return image, img_sublabel
 
 
-def standardize(img, size=(128,128)):
-    rescaled = cv2.resize(img, size, interpolation=cv2.INTER_AREA)
-    standardized = (rescaled - np.mean(rescaled)) / np.std(rescaled)
-    return standardized
+# Load and normalize image
+def load_img(img_path):
+    image = cv2.imread(img_path)
+    return normed
 
 
-def load_dataset(ratio=0.8):
-    dataset = pd.read_csv(DATASET_PATH, header=None, names=["img_name", "label", "sublabel"])
-    bad_set = dataset[dataset.label == "None"]
+# Load and split dataset
+def load_dataset(ratio=0.8, transform=TRANSFORM, label=True):
+    columns = ["img_name", "label", "sublabel"]
+    dataset = pd.read_csv(DATASET_PATH, header=None, names=columns)
+    bad_set = DermDataset(dataset[dataset.label == "None"], 
+        transform=transform, label=label)
     dataset = dataset[dataset.label != "None"]
-    rand_idxs = np.random.choice(a=[True, False], size=len(dataset.index), p=[ratio, 1-ratio])
-    train_set = dataset[rand_idxs]
-    test_set = dataset[np.invert(rand_idxs)]
-    return DermDataset(train_set), DermDataset(test_set), DermDataset(bad_set)
+    rand_idxs = np.random.choice(a=[True, False], size=len(dataset.index), 
+        p=[ratio, 1-ratio])
+    train_set = DermDataset(dataset[rand_idxs], 
+        transform=transform, label=label)
+    test_set = DermDataset(dataset[np.invert(rand_idxs)], 
+        transform=transform, label=label)
+    return train_set, test_set, bad_set
+
--- microservice/training/vae.py
+++ microservice/training/vae.py
@@ -1,73 +1,103 @@
 import torch
+from torch.autograd import Variable
 import torch.nn as nn
-import torch.autograd as autograd
+import torch.nn.functional as F
+import torchvision.models as models
+import torchvision.transforms as T
 
 
 # VAE Encoder
 class Encoder(nn.Module):
-    def __init__(self, in_dim, hidden_dim, latent_dim):
-        super().__init__()
-        self.in_dim = in_dim
+    def __init__(self, hidden_dim, latent_dim):
+        super(Encoder, self).__init__()
+        # Use pretrained resnet model, detach last layer
+        resnet = models.resnet50(pretrained=True)
+        modules = list(resnet.children())[:-1]
+        self.resnet = nn.Sequential(*modules)
         # Intermediate linear transformation
-        self.f_hidden = nn.Linear(in_dim, hidden_dim)
+        self.fc = nn.Linear(resnet.fc.in_features, hidden_dim)
+        self.bn = nn.BatchNorm1d(hidden_dim, momentum=0.01)
         # Latent space parameters
-        self.f_z_mean = nn.Linear(hidden_dim, latent_dim)
-        self.f_z_log_sigma = nn.Linear(hidden_dim, latent_dim)
+        self.fc_mean = nn.Linear(hidden_dim, latent_dim)
+        self.fc_log_sigma = nn.Linear(hidden_dim, latent_dim)
         # Activation function
         self.relu = nn.ReLU()
 
     def forward(self, x):
-        h = self.relu(self.f_hidden(x))
-        z_mean = self.f_z_mean(h)
-        z_log_sigma = self.f_z_log_sigma(h)
+        x = self.resnet(x).view(x.size(0), -1)
+        x = self.relu(self.bn(self.fc(x)))
+        z_mean = self.fc_mean(x)
+        z_log_sigma = self.fc_log_sigma(x)
         return z_mean, z_log_sigma
 
 
 # VAE Decoder
 class Decoder(nn.Module):
-    def __init__(self, latent_dim, hidden_dim, out_dim):
-        super().__init__()
+    def __init__(self, latent_dim, hidden_dim):
+        super(Decoder, self).__init__()
         # Intermediate linear transformation
-        self.f_hidden = nn.Linear(latent_dim, hidden_dim)
-        # Reconstruct original input
-        self.f_decode = nn.Linear(hidden_dim, out_dim)
+        self.fc1 = nn.Linear(latent_dim, hidden_dim)
+        self.bn1 = nn.BatchNorm1d(hidden_dim, momentum=0.01)
+        self.fc2 = nn.Linear(hidden_dim, 64 * 4 * 4)
+        self.bn2 = nn.BatchNorm1d(64 * 4 * 4, momentum=0.01)
+        # Reconstruct original input with deconvolutional layers
+        self.convTranspose1 = nn.Sequential(
+            nn.ConvTranspose2d(in_channels=64, out_channels=16,
+                kernel_size=(3, 3), stride=(2, 2), padding=(0, 0)
+            ),
+            nn.BatchNorm2d(16, momentum=0.01),
+            nn.ReLU()
+        )
+        self.convTranspose2 = nn.Sequential(
+            nn.ConvTranspose2d(in_channels=16, out_channels=8,
+                kernel_size=(3, 3), stride=(2, 2), padding=(0, 0)
+            ),
+            nn.BatchNorm2d(8, momentum=0.01),
+            nn.ReLU()
+        )
+        self.convTranspose3 = nn.Sequential(
+            nn.ConvTranspose2d(in_channels=8, out_channels=3,
+                kernel_size=(3, 3), stride=(2, 2), padding=(0, 0)
+            ),
+            nn.BatchNorm2d(3, momentum=0.01)
+        )
         # Activation functions
         self.relu = nn.ReLU()
-        self.sigmoid = nn.Sigmoid()
     
-    def forward(self, z):
-        h = self.relu(self.f_hidden(z))
-        x = self.sigmoid(self.f_decode(h))
+    def forward(self, x):
+        x = self.relu(self.bn1(self.fc1(x)))
+        x = self.relu(self.bn2(self.fc2(x)))
+        x = x.view(-1, 64, 4, 4)
+        x = self.convTranspose1(x)
+        x = self.convTranspose2(x)
+        x = self.convTranspose3(x)
+        x = F.interpolate(x, size=(224, 224), mode='bilinear', 
+            align_corners=False)
         return x
 
 
 # VAE
 class VAE(nn.Module):
-    def __init__(self, size, hidden_dim=None, latent_dim=None):
+    def __init__(self, hidden_dim=None, latent_dim=None):
         super().__init__()
-        self.size = size
-        self.in_dim = size[0] * size[1]
-        if hidden_dim:
-            self.hidden_dim = hidden_dim
-        else:
-            self.hidden_dim = self.in_dim // 2
-        if latent_dim:
-            self.latent_dim = latent_dim
-        else:
-            self.latent_dim = self.hiden_dim // 2
+        self.hidden_dim = hidden_dim
+        self.latent_dim = latent_dim
 
         # Set up encoder and decoder
-        self.encoder = Encoder(in_dim, hidden_dim, latent_dim)
-        self.decoder = Decoder(latent_dim, hidden_dim, in_dim)
+        self.encoder = Encoder(self.hidden_dim, self.latent_dim)
+        self.decoder = Decoder(self.latent_dim, self.hidden_dim)
 
-    def sampling(self, z_params):
-        z_mean, z_log_sigma = z_params
-        epsilon = autograd.Variable(torch.randn(z_mean.size()))
-        return z_mean + torch.exp(z_log_sigma) * epsilon
+    def sample(self, z_mean, z_log_sigma):
+        if self.training:
+            std = z_log_sigma.exp_()
+            epsilon = Variable(torch.randn(z_mean.size()))
+            return epsilon.mul(std).add_(z_mean)
+        else:
+            return z_mean
 
     def forward(self, x):
-        x = x.reshape(-1, self.in_dim)
         z_params = self.encoder(x)
-        z = self.sampling(z_params)
-        x_res = self.decoder(z)
-        return x_res.reshape(self.size)
+        z_mean, z_log_sigma = z_params
+        z = self.sample(z_mean, z_log_sigma)
+        x_rec = self.decoder(z)
+        return x_rec, z_mean, z_log_sigma
