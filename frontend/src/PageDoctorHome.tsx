9164eafe525acc057aa204b9214c1206c8a0fdad
TankMissile <alecfeldman@disroot.org>
2020-08-12 18:30:01 -0400

    Improved syntax consistency and organization of all files.

--- frontend/src/PageDoctorHome.tsx
+++ frontend/src/PageDoctorHome.tsx
@@ -1,17 +1,17 @@
-import React from "react"
-import Footer from "./Footer"
-import DoctorData from "../TestRequests/doctor.json"
+import React from 'react'
+import Footer from './Footer'
+import DoctorData from '../TestRequests/doctor.json'
 
 function DoctorHome() {
     return(
-        <div className="doctor-home">
-            <header>
-                <h2 className="welcome">Welcome {DoctorData.firstName} {DoctorData.lastName}</h2>
-            </header>
-            <button className="add-appointments" type="button">Add Appointments</button>
-            <button className="callendy" type="button">Link with Callendy</button>
-            <Appointments />
-            <Footer />
+        <div className='doctor-home'>
+          <header>
+            <h2 className='welcome'>Welcome {DoctorData.firstName} {DoctorData.lastName}</h2>
+          </header>
+          <button className='add-appointments' type='button'>Add Appointments</button>
+          <button className='callendy' type='button'>Link with Callendy</button>
+          <Appointments />
+          <Footer />
         </div>
     )
 }
@@ -20,18 +20,18 @@ function Appointments() {
     let appointments = {}
 
     if (!DoctorData.appointments.length) {
-        appointments = <a className="no-appointments" href="">No appointments</a>
+        appointments = <a className='no-appointments' href=''>No Appointments</a>
     } else {
         appointments = DoctorData.appointments.map(
             user => {
-                <div className="appointment" key={user.id}>{user.firstName} {user.lastName}</div>
+                <div className='appointment' key={user.id}>{user.firstName} {user.lastName}</div>
             }
         )
     }
 
     return(
         <div>
-            {appointments}
+          {appointments}
         </div>
     )
 }
