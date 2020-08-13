ea55a7fd4836284b8efb3b52d1788fc7c2b00519
AlecFeldman <afeldman_01@arcadia.edu>
2020-08-13 18:51:28 -0400

    Fix error function after downgrading react-bootstrap.

--- frontend/src/components/ErrorFunction.tsx
+++ frontend/src/components/ErrorFunction.tsx
@@ -1,13 +1,13 @@
 import React from 'react'
-import Alert from 'react-bootstrap/Alert'
+import { Alert } from 'react-bootstrap'
 import InvalidLogin from '../../TestRequests/invalidlogin.json'
 import '../styles/AlertStyle.css'
 
 function ErrorFunction() {
     return(
         <div className='alert'>
-          <Alert variant='danger'>
-            <Alert.Heading>{InvalidLogin.message}</Alert.Heading>
+          <Alert bsStyle='danger'>
+            <h4>{InvalidLogin.message}</h4>
           </Alert>
         </div>
     )
