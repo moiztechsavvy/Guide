4482f215e00251ae6d34c913ea6a769c2b754208
tontiveros <tontiveros@arcadia.edu>
2020-08-17 19:11:15 -0500

    Calendar Page w/th Compiler Error of @fullCalendar/daygrid Module not found

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -3,10 +3,12 @@ import ReactDOM from 'react-dom'
 import Home from './pages/Home'
 import * as serviceWorker from './functions/serviceWorker'
 import './styles/index.css'
+import DemoApp from './calendar'
 
 ReactDOM.render(
     <React.StrictMode>
       <Home />
+      <DemoApp />
     </React.StrictMode>,
     document.getElementById('root')
 )
