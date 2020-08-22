f8258988f441f0bb98fe459ccaa9a0b59faad846
Ubuntu <ubuntu@ip-172-31-64-65.ec2.internal>
2020-08-22 00:09:35 +0000

    Login

--- frontend/src/pages/Signup.tsx
+++ frontend/src/pages/Signup.tsx
@@ -1,62 +1,81 @@
 import React from 'react'
 import axios from 'axios'
+import {ButtonToolbar, SplitButton, MenuItem} from 'react-bootstrap'
 import Validate from '../functions/Validate'
 import '../styles/Login.css'
 
-class Signup extends React.Component<{}, {emailInput: string, passwordInput: string, phoneInput: string, zipInput: string}> {
+var PostLink = 'patient/signup'
+
+class Signup extends React.Component<{}, {email: string, password: string, phone: string, zip: string}> {
+  
     constructor(props: string) {
         super(props)
-        this.state = {emailInput:'', passwordInput: '', phoneInput:'', zipInput:''}
+        this.state = {email:'', password: '', phone:'', zip:''}
         this.handleEmail = this.handleEmail.bind(this)
         this.handlePassword = this.handlePassword.bind(this)
         this.handlePhone = this.handlePhone.bind(this)
         this.handleZip = this.handleZip.bind(this)
+        this.handleDDrop = this.handleDDrop.bind(this)
+        this.handlePDrop = this.handlePDrop.bind(this)
     }
 
     handleEmail(event: any) {
-        this.setState({emailInput: event.target.value})
-        Validate(this.state.emailInput, 'email')
+        this.setState({email: event.target.value})
+        Validate(this.state.email, 'email')
     }
 
     handlePassword(event: any) {
-        this.setState({passwordInput: event.target.value})
-        Validate(this.state.passwordInput, 'pass')
+        this.setState({password: event.target.value})
+        Validate(this.state.password, 'pass')
     }
 
     handlePhone(event: any) {
-        this.setState({phoneInput: event.target.value})
-        Validate(this.state.phoneInput, 'phone')
+        this.setState({phone: event.target.value})
+        Validate(this.state.phone, 'phone')
     }
 
     handleZip(event: any) {
-        this.setState({zipInput: event.target.value})
-        Validate(this.state.zipInput, 'zip')
+        this.setState({zip: event.target.value})
+        Validate(this.state.zip, 'zip')
     }
 
     handleSubmit() {
-        axios.post('3.224.66.200:5000/auth', this.state.emailInput)
-        axios.post('3.224.66.200:5000/auth', this.state.passwordInput)
-        axios.post('3.224.66.200:5000/auth', this.state.phoneInput)
-        axios.post('3.224.66.200:5000/auth', this.state.zipInput)
+        axios.post('3.224.66.200:5000/auth/'+PostLink, this.state)
+    }
+
+    handlePDrop(){
+      PostLink = 'patient/signup'
+    }
+
+    handleDDrop(){
+      PostLink = 'doctor/signup'
     }
 
     render() {
         return (
+          <div>
+            <ButtonToolbar>
+              <SplitButton title='Sign Up Options' pullRight id='split-button-pull-right' bsStyle='primary'>
+                  <MenuItem eventKey='1' onClick={this.handlePDrop}>Patient Signin</MenuItem>
+                  <MenuItem eventKey='2' onClick={this.handleDDrop}>Doctor Signin</MenuItem>
+              </SplitButton>
+            </ButtonToolbar>
             <form>
               <div className='form-group'>
-                <input type='email' className='form-control' id='exampleInputEmail1' aria-describedby='emailHelp' placeholder='Email' value={this.state.emailInput} onChange={this.handleEmail}></input>
+                <input type='email' className='form-control' id='exampleInputEmail1' aria-describedby='emailHelp' placeholder='Email' value={this.state.email} onChange={this.handleEmail}></input>
               </div>
               <div className='form-group'>
-                <input type='password' className='form-control' id='exampleInputPassword1' placeholder='Password' value={this.state.passwordInput} onChange={this.handlePassword}></input>
+                <input type='password' className='form-control' id='exampleInputPassword1' placeholder='Password' value={this.state.password} onChange={this.handlePassword}></input>
               </div>
               <div className='form-group'>
-                <input type='tel' className='form-control' id='exampleInputPhone1' placeholder='Phone Number' value={this.state.phoneInput} onChange={this.handlePhone}></input>
+                <input type='tel' className='form-control' id='exampleInputPhone1' placeholder='Phone Number' value={this.state.phone} onChange={this.handlePhone}></input>
               </div>
               <div className='form-group'>
-                <input type='number' className='form-control' id='exampleInputZipcode1' placeholder='Zipcode' value={this.state.zipInput} onChange={this.handleZip}></input>
+                <input type='number' className='form-control' id='exampleInputZipcode1' placeholder='Zipcode' value={this.state.zip} onChange={this.handleZip}></input>
               </div>
               <button type='submit' className='btn btn-primary' onClick={this.handleSubmit}>Submit</button>
             </form>
+          </div>
         )
     }
 }
